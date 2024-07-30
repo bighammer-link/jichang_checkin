1bc049df0e0e127ad1dabae00ffe4f6996c6fdbc
yeqiu6080 <87291369+yeqiu6080@users.noreply.github.com>
2024-07-30 14:32:51 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -4,40 +4,56 @@ session = requests.session()
 # 机场的地址
 url = os.environ.get('URL')
 # 配置用户名（一般是邮箱）
-email = os.environ.get('EMAIL')
-# 配置用户名对应的密码 和上面的email对应上
-passwd = os.environ.get('PASSWD')
+
+config = os.environ.get('CONFIG')
 # server酱
 SCKEY = os.environ.get('SCKEY')
 
 login_url = '{}/auth/login'.format(url)
 check_url = '{}/user/checkin'.format(url)
 
-
-header = {
+def sign(order,user,pwd):
+        global url,SEKEY
+        header = {
         'origin': url,
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
-}
-data = {
-        'email': email,
-        'passwd': passwd
-}
-try:
-    print('进行登录...')
-    response = json.loads(session.post(url=login_url,headers=header,data=data).text)
-    print(response['msg'])
-    # 进行签到
-    result = json.loads(session.post(url=check_url,headers=header).text)
-    print(result['msg'])
-    content = result['msg']
-    # 进行推送
-    if SCKEY != '':
-        push_url = 'https://sctapi.ftqq.com/{}.send?title=机场签到&desp={}'.format(SCKEY, content)
-        requests.post(url=push_url)
-        print('推送成功')
-except:
-    content = '签到失败'
-    print(content)
-    if SCKEY != '':
-        push_url = 'https://sctapi.ftqq.com/{}.send?title=机场签到&desp={}'.format(SCKEY, content)
-        requests.post(url=push_url)
+        }
+        data = {
+        'email': user,
+        'passwd': pwd
+        }
+        try:
+                print(f'===账号{order}进行登录...===')
+                response = json.loads(session.post(url=login_url,headers=header,data=data).text)
+                print(response['msg'])
+                # 进行签到
+                result = json.loads(session.post(url=check_url,headers=header).text)
+                print(result['msg'])
+                content = result['msg']
+                # 进行推送
+                if SCKEY != '':
+                        push_url = 'https://sctapi.ftqq.com/{}.send?title=机场签到&desp={}'.format(SCKEY, content)
+                        requests.post(url=push_url)
+                        print('推送成功')
+        except:
+                content = '签到失败'
+                print(content)
+                if SCKEY != '':
+                        push_url = 'https://sctapi.ftqq.com/{}.send?title=机场签到&desp={}'.format(SCKEY, content)
+                        requests.post(url=push_url)
+                        print('推送成功')
+        print('===账号{order}签到结束===\n'.format(order=order))
+if __name__ == '__main__':
+        configs = config.splitlines()
+        if len(configs) %2 == 0 or len(configs) == 0:
+                print('配置文件格式错误')
+                exit()
+        user_quantity = len(configs)+1
+        user_quantity = user_quantity // 2
+        for i in range(user_quantity):
+                t = i*2
+                t -= 1
+                user = configs[t]
+                pwd = configs[i*2]
+                sign(i,user,pwd)
+        
